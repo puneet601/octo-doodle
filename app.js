b68cd6447893070da5020de8cc7cb407af16b44a
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-09-13 12:24:46 +0530

    ADDED enviornment vars

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+require("dotenv").config();
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+const encrypt=require("mongoose-encryption");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+const userSchema=new mongoose.Schema({
+     name: {
+          type: String,
+          required: true
+        },
+     email:{type:String,
+     required:true},
+     password: {
+          type: String,
+          required: true
+        }
+});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]}); //before creating model after schema
+const Users=new mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+     res.render("home");
+});
+app.get("/login",function(req,res){
+     res.render("login");
+});
+app.get("/register",function(req,res){
+    res.render("register");
+});
+app.post("/register",function(req,res){
+ const newUser=new Users({
+      name:req.body.name,
+      email:req.body.username,
+      password:req.body.password
+ });
+ newUser.save(function(err){
+      if(err)
+      console.log(err);
+      else
+      res.render("secrets");
+ });
+});
+app.post("/login",function(req,res){
+     const username=req.body.username;
+     const password=req.body.password;
+     Users.findOne({email:username},function(err,foundUser){
+     if(err)
+     console.log(err);
+     else{
+          if(foundUser.password === password)
+          res.render("secrets");
+     }
+});
+});
+app.listen(3000);
