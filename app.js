f943dd3799dae253aae60fea22fd4e35cbb36a0a
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-10-18 14:32:20 +0530

    make it better

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-mongoose.connect( process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-Puneet:Springday1!@cluster0.ergrz.mongodb.net/secrets",{useNewUrlParser:true,useUnifiedTopology: true });
 mongoose.set("useCreateIndex",true);
 
 const userSchema=new mongoose.Schema({
